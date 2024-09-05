09a1e6d397e31bc802f3dabf0f8e89b65bbdaa60
E. Keys <e.t.keys@gmail.com>
2024-09-04 20:41:57 -0400

    implemenent concrete models
    Created a DownloadItem model so that we can get closer to how we'd
    represent and actual database.
    
    Also created a DownloadSet model to tie many downloads to.

--- src/App/routes/todo.py
+++ src/App/routes/todo.py
@@ -1,40 +1,42 @@
 from flask import abort, Blueprint, flash, redirect, render_template, url_for
 
 from App.forms.todo import DownloadItemDetailsForm
+from App.models import DownloadItem, DownloadSet
 
 todo_blueprint = Blueprint('todo', __name__, url_prefix='/todo')
 
+user_download_set = DownloadSet()
+
 user_todo = [
-        { 'Id': 1,
-         'Url': 'https://example.com/1',
-         'Title': 'Video #1',
-         'AudioOnly': True},
-        { 'Id': 10,
-         'Url': 'https://example.com/2',
-         'Title': 'Video #2',
-         'AudioOnly': False},
-        { 'Id': 100,
-         'Url': 'https://example.com/3',
-         'Title': 'Video #3',
-         'AudioOnly': False}
+    DownloadItem(
+        download_set_id=user_download_set.id,
+        url='https://example.com/1',
+        title='Video #1',
+        audio_only=True),
+    DownloadItem(
+        download_set_id=user_download_set.id,
+        url='https://example.com/2',
+        title='Video #2'),
+    DownloadItem(
+        download_set_id=user_download_set.id,
+        url='https://example.com/3',
+        title='Video #3')
 ]
 
 @todo_blueprint.route('/add', methods=['GET', 'POST'])
 def add_item():
     form = DownloadItemDetailsForm()
     if form.validate_on_submit():
-        next_id = max([i['Id'] for i in user_todo]) * 10
-
         # TODO Make safe characters in title
 
-        new_item = {
-            'Id': next_id,
-            'Title': form.title.data,
-            'AudioOnly': form.audio_only.data,
-            'Url': form.url.data,
-        }
+        new_item = DownloadItem(
+            download_set_id=user_download_set.id,
+            url=form.url.data,
+            title=form.title.data,
+            audio_only=form.audio_only.data)
         user_todo.append(new_item)
-        flash(f"Item '{new_item['Title']}' added successfully.", category="success")
+
+        flash(f"Item added successfully.", category="success")
         return redirect(url_for('todo.display_todo'))
     else:
         return render_template('todo_add_edit_item.html', form=form, title="Add Item")
@@ -44,14 +46,14 @@ def display_todo():
     return render_template('todo.html', todo=user_todo)
 
 @todo_blueprint.get('/<id>/edit')
-def edit_item(id: int):
+def edit_item(id):
     try:
-        item = next(i for i in user_todo if i['Id'] == int(id))
+        item = next(i for i in user_todo if i.id == id)
 
         form = DownloadItemDetailsForm()
-        form.title.data = item['Title']
-        form.audio_only.data = item['AudioOnly']
-        form.url.data = item['Url']
+        form.title.data = item.title
+        form.audio_only.data = item.audio_only
+        form.url.data = item.url
 
         return render_template(
             'todo_add_edit_item.html',
@@ -62,15 +64,15 @@ def edit_item(id: int):
         abort(404, description=f"Could not find item with Id of '{id}'.")
 
 @todo_blueprint.post('/<id>/edit')
-def edit_item_submit(id: int):
+def edit_item_submit(id):
     form = DownloadItemDetailsForm()
     if form.validate_on_submit():
         try:
-            item = next(i for i in user_todo if i['Id'] == int(id))
+            item = next(i for i in user_todo if i.id == id)
 
-            item['Title'] = form.title.data
-            item['AudioOnly'] = form.audio_only.data
-            item['Url'] = form.url.data
+            item.title = form.title.data
+            item.audio_only = form.audio_only.data
+            item.url = form.url.data
 
             flash('Item updated successfully.', category="success")
             return redirect(url_for('todo.display_todo'))
@@ -101,14 +103,14 @@ def delete_all():
 
 
 @todo_blueprint.get('/<id>/delete')
-def confirm_delete_item(id: int):
+def confirm_delete_item(id):
     try:
-        item = next(i for i in user_todo if i['Id'] == int(id))
+        item = next(i for i in user_todo if i.id == id)
 
         form = DownloadItemDetailsForm(True)
-        form.title = item['Title']
-        form.audio_only = item['AudioOnly']
-        form.url = item['Url']
+        form.title = item.title
+        form.audio_only = item.audio_only
+        form.url = item.url
 
         return render_template(
             'todo_delete_item.html',
@@ -118,11 +120,11 @@ def confirm_delete_item(id: int):
         abort(404, description=f"Could not find item with Id of '{id}'.")
 
 @todo_blueprint.post('/<id>/delete')
-def delete_item(id: int):
+def delete_item(id):
     form = DownloadItemDetailsForm(True)
     if form.submit.data:
         try:
-            idx = next(i for i,x in enumerate(user_todo) if x['Id'] == int(id))
+            idx = next(i for i,x in enumerate(user_todo) if x.id == id)
             del user_todo[idx]
         except StopIteration:
             abort(404, description=f"Could not find item with Id of '{id}'.")
